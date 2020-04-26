eebd60ddded40e0734484b1c2fc8464f4aa98dd9
bartbutenaers <bart.butenaers@gmail.com>
2020-04-26 21:58:45 +0200

    Initial commit

---
+++ languages/fr.js
@@ -0,0 +1,38 @@
+module.exports = {
+    descriptions: {
+        "N":     "nord",
+        "NNE":   "nord-nord-est",
+        "NE":    "nord-est",
+        "ENE":   "est-nord-est",
+        "E":     "est",
+        "ESE":   "est-sud-est",
+        "SE":    "sud-est",
+        "SSE":   "sud-sud-est",
+        "S":     "sud",
+        "SSW":   "sud-sud-ouest",
+        "SW":    "sud-ouest",
+        "WSW":   "ouest-sud-ouest",
+        "W":     "ouest",
+        "WNW":   "ouest-nord-ouest",
+        "NW":    "nord-ouest",
+        "NNW":   "nord-nord-ouest"
+    },
+    abbreviations: {
+        "N":     "N",
+        "NNE":   "NNE",
+        "NE":    "NE",
+        "ENE":   "ENE",
+        "E":     "E",
+        "ESE":   "ESE",
+        "SE":    "SE",
+        "SSE":   "SSE",
+        "S":     "S",
+        "SSW":   "SSO",
+        "SW":    "SO",
+        "WSW":   "OSO",
+        "W":     "O",
+        "WNW":   "ONO",
+        "NW":    "NO",
+        "NNW":   "NNO"
+    }
+}
