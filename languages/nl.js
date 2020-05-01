bb0b8a15327aa5dca3522adbb4412155f57b5400
bartbutenaers <bart.butenaers@gmail.com>
2020-05-01 16:57:02 +0200

    Update

--- languages/nl.js
+++ languages/nl.js
@@ -1,21 +1,22 @@
+// Source https://vrttaal.net/taaladvies-spelling/windrichtingen
 module.exports = {
     descriptions: {
-        "N":     "Noord",
-        "NNE":   "Noord-noordoost",
-        "NE":    "Noordoosten",
-        "ENE":   "Oost-noordoost",
-        "E":     "Oost",
-        "ESE":   "Oost-zuidoost",
-        "SE":    "Zuidoosten",
-        "SSE":   "Zuid-zuidoosten",
-        "S":     "Zuid",
-        "SSW":   "Zuid-zuidwesten",
-        "SW":    "Zuidwesten",
-        "WSW":   "West-zuidwesten",
-        "W":     "West",
-        "WNW":   "West-noordwesten",
-        "NW":    "Noordwesten",
-        "NNW":   "Noord-noordwesten"
+        "N":     "noord",
+        "NNE":   "noordnoordoost",
+        "NE":    "noordoost",
+        "ENE":   "oostnoordoost",
+        "E":     "oost",
+        "ESE":   "oostzuidoost",
+        "SE":    "zuidoost",
+        "SSE":   "zuidzuidoost",
+        "S":     "zuid",
+        "SSW":   "zuidzuidwest",
+        "SW":    "zuidwest",
+        "WSW":   "westzuidwest",
+        "W":     "west",
+        "WNW":   "westnoordwest",
+        "NW":    "noordwest",
+        "NNW":   "noordnoordwest"
     },
     abbreviations: {
         "N":     "N",
